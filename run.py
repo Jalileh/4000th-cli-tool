8c3df1f6300e19820dbf03c33506166bf0aef35d
Jalileh <fayardwatson@outlook.com>
2024-12-30 16:21:45 +0100

    AutoCommit main/#6

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import os, sys, re as rgx
-from typing import Tuple
+from typing import List, Tuple
 current_dir = os.getcwd()
 
 import sqlite3 as sql
 
+class Repositories:
+  def __init__(self, name, path):
+        self.name = name
+        self.path = path
  
 def TerminatePy(err = 0):
     exit(err)
@@ -31,9 +35,19 @@ databasePath = f'{GetPWD()}/data/repo-brain.db'
 con = sql.connect(f'{databasePath}')
 
 def SavedDirTravel():
-    repoId, args = getArg(1)
+    
+    arg_one, args = getArg(1)
+    if arg_one == "list":
+        DisplayRepositories();
+        TerminatePy();
+    elif arg_one == "delete":
+        DbDeleteRepoRow( args[2])
+        TerminatePy();
+        
+        
+    
     if len(args) > 2:
-        newRepoRegistry = args[1];
+        newRepoRegistry = arg_one;
         if args[2] == 'add':
             print("Registering new Repo Name:", newRepoRegistry)
             err = DbSaveCurrentRepo(newRepoRegistry)
@@ -43,15 +57,45 @@ def SavedDirTravel():
         else:
             return print("Use add or remove");
     else:
-        repoPath, err = DbGetRepo(repoId)
+        repoPath, err = DbGetRepo(arg_one)
         EnterNewShellAndExit(repoPath)
         if err: print(err)
         
             
 
     
-     
+def DisplayRepositories():
+    err, Repositories = DbGetRepoList();
+    if False == err:
+        for repo in Repositories:
+            print(f'Access Name: {repo.name}, Path: {repo.path}')
+            
+            
+    else:
+        print("error db-repository list")
+        
     
+    
+def DbDeleteRepoRow(Name):
+       tableName = 'repositories'
+       con.execute(f"DELETE FROM {tableName} where name = ?", [Name]);
+       con.commit()
+    
+def DbGetRepoList() -> tuple[bool, list] :
+    repositories = [Repositories]
+    tableName = 'repositories'
+    try:
+       res = con.execute(f"SELECT * FROM {tableName}");
+       con.commit()
+       rows = res.fetchall()
+       repositories = [Repositories(name=row[0], path=row[1]) for row in rows]
+       return False, repositories
+           
+    except Exception as e:
+        print(e)
+        return True, repositories
+    
+    return repositories, True;
     
 def DbGetRepo(repoId) -> tuple[str, Exception | bool]:
      repositoryPath = ""
